a8afd35aac54dab20f311f48f142c950d61acdda
dongqunren <chenjunrenyx@163.com>
2021-12-07 20:07:58 +0800

    更新代码

--- base/magic_method_attr.py
+++ base/magic_method_attr.py
@@ -9,37 +9,65 @@ class AttrMethod:
         self.age = age
         print(self.__dict__)
 
-    def __setattr__(self, key, value):
-        print("*" * 50)
-        print("setting:{},  with:{}".format(key, value))
-        print("current __dict__ : {}".format(self.__dict__))
-        self.__dict__[key] = value
+    # def __setattr__(self, key, value):
+    #     print("*" * 50)
+    #     print("setting:{},  with:{}".format(key, value))
+    #     print("current __dict__ : {}".format(self.__dict__))
+    #     self.__dict__[key] = value
 
-    def __getattr__(self, item):
-        if item in self.__dict__:
-            return self[item]
-        return None
+    # def __getattr__(self, item):
+    #     if item in self.__dict__:
+    #         return self[item]
+    #     return None
 
-    def set(self, aa, bb):
-        x = aa
-        aa = bb
-        bb = x
-        print("aa: %s, bb: %s" % (aa, bb))
+    # def __getattribute__(self, item):
+    #     print("开始属性校验拦截功能")
+    #     print(item)
+    #     if item == "name":
+    #         print("现在开始调用的是name属性")
+    #     elif item == "age":
+    #         print("现在开始调用的是age属性")
+    #     else:
+    #         print("现在开始调用的是其他属性")
+    #     return super(AttrMethod, self).__getattribute__(item)
+
+    # def my_set(self, aa, bb):
+    #     x = aa
+    #     aa = bb
+    #     bb = x
+    #     print("aa: %s, bb: %s" % (aa, bb))
+
+    def __dir__(self):
+        return self.__dict__
+
+    def __get__(self, instance, owner):
+        print("__get__")
+
+    def __set__(self, instance, value):
+        print("__set__")
+
+    def __delattr__(self, item):
+        print("__delattr__")
+
+    def __delete__(self, instance):
+        print("__delete__")
 
 
 if __name__ == '__main__':
     a = AttrMethod('Jojo', '18')
     print("*" * 50)
     print(a.name)
-    print(a.sex)
+    # print(a.sex)
 
-    setattr(a, 'asf', {'a': 1})
-    print(a.asf)
+    # setattr(a, 'asf', {'a': 1})
+    # print(a.asf)
 
-    print("*" * 50)
-    print(a.__dict__)
-    c = getattr(a, 'set')
-    c('1', '2')
+    print(dir(a))
+
+    # print("*" * 50)
+    # print(a.__dict__)
+    # c = getattr(a, 'my_set')
+    # c('1', '2')
 
     # setattr(a, 'name', 'bin')
     # print(getattr(a, 'name'))
