5f70175ec2d43aef5f6ea583fbeeca93317f55d2
dongqunren <chenjunrenyx@163.com>
2021-12-10 17:49:36 +0800

    更新代码

--- base/magic_method_math.py
+++ base/magic_method_math.py
@@ -6,3 +6,43 @@ class MathMethod:
 
     def __add__(self, other):
         return self.num + other
+
+    def __sub__(self, other):
+        return self.num - other
+
+    def __mul__(self, other):
+        return self.num * other
+
+    def __truediv__(self, other):
+        return self.num / other
+
+    def __floordiv__(self, other):
+        return self.num / other
+
+    def __mod__(self, other):
+        return self.num % other
+
+    def __divmod__(self, other):
+        return self.num % other
+
+    def __pow__(self, power, modulo=None):
+        return self.num ** power
+
+    def __lshift__(self, other):
+        return self.num << other
+
+    def __rshift__(self, other):
+        return self.num >> other
+
+    def __and__(self, other):
+        return self.num & other
+
+    def __xor__(self, other):
+        return self.num ^ other
+
+    def __or__(self, other):
+        return self.num | other
+
+
+if __name__ == '__main__':
+    a = MathMethod(12)
